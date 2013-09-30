10015d53a9f528faaa1b78f6d36ad525748b96ba
Lei Wang <lwang@epsilon.com>
2013-09-29 21:32:13 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,38 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: Lei Wang
+
+# EX2.1: 0 indicates octal numeral value
+# 02132 = 2 + 3 * 8 + 1 * (8**2) + 2 * (8**3) = 1114
+# 02492: octal numeral system uses only digits 0 to 7,  so 9 is invalid token
+
+# EX2.2: before modifying the code, it would produce no output
+# here's the modified script
+print 5
+x = 5
+print x+1
+
+# EX2.3
+# width/2 = 8 int
+# width/2.0 = 8.5 float
+# height/3 = 4.0 float
+# 1 + 2 * 5 = 11 int
+# delimiter * 5 = '.....' str
+
+# EX2.4 - 1
+import math
+r = 5
+volume = 4.0/3 * math.pi * r**3
+print volume
+
+# EX2.4 - 2
+cost = 24.95 * (1-.4) * 60 + 3 + .75 * (60-1)
+print cost
+
+# EX2.4 - 3
+running = (8*60+15)*2 + (7*60+12)*3
+second = running - (running/60) * 60
+minute = 52 + (running/60)
+hour = 6 + minute/60
+minute = minute - minute/60 * 60
+print str(hour)+':' + str(minute) + ':' + str(second)
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,36 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Lei Wang
+
+# EX3.1
+# will get "Name Error: name 'repeat_lyrics' is not defined"
+
+# EX3.2
+# will get same (correct) output as the original script
+
+# EX3.3
+def right_justify(s):
+	print ' '*(70 - len(s)) + s
+
+right_justify('allen')
+
+# EX3.4 
+def do_twice(f, v):
+	f(v)
+	f(v)
+
+def print_twice(s):
+	print s
+	print s
+
+print 'Now we call do_twice twice'
+do_twice(print_twice, 'spam')
+do_twice(print_twice, 'spam')
+
+def do_four(f, v):
+	do_twice(f,v)
+	do_twice(f,v)
+
+print 'Now we call do_four'
+do_four(print_twice, 'spam')	
+
